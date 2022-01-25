82e5819300a187d3c9f834ca77558f65ae77d268
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-01-25 02:58:45 -0300

    criando projeto aspnet core e migrations

---
+++ APICatalogo/Migrations/20220125053817_Initial.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APICatalogo.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categorias",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(maxLength: 80, nullable: false),
+                    ImagemUrl = table.Column<string>(maxLength: 200, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categorias", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(maxLength: 80, nullable: false),
+                    Descricao = table.Column<string>(maxLength: 200, nullable: false),
+                    Preco = table.Column<decimal>(nullable: false),
+                    ImageUrl = table.Column<string>(maxLength: 200, nullable: false),
+                    Estoque = table.Column<float>(nullable: false),
+                    DataCadastro = table.Column<DateTime>(nullable: false),
+                    CategoriaId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Produtos_Categorias_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categorias",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Produtos_CategoriaId",
+                table: "Produtos",
+                column: "CategoriaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+
+            migrationBuilder.DropTable(
+                name: "Categorias");
+        }
+    }
+}
---
+++ APICatalogo/Models/Produto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace APICatalogo.Models
+{
+    [Table("Produtos")]
+    public class Produto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(80)]
+        public string Nome { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string Descricao { get; set; }
+        [Required]
+        public decimal Preco { get; set; }
+        [Required]
+        [MaxLength(200)]
+        public string ImageUrl { get; set; }
+        public float Estoque { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
