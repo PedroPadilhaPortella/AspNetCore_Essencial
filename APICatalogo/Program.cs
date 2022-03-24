0b908174b5a7b427539cb377074f9994dfd4bd5b
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-03-24 13:02:38 -0300

    finalizando projeto

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -21,6 +21,7 @@ namespace APICatalogo
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
+                    webBuilder.UseIISIntegration();
                 });
     }
 }
